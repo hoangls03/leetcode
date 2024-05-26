0a0c8340ca04bda7fe787bfbae6c558df166548a
hoangls03 <168925511+hoangls03@users.noreply.github.com>
2024-05-26 02:31:19 +0000

    [LeetCode Sync] Runtime - 9 ms (44.90%), Memory - 41.2 MB (9.09%)

---
+++ LeetCode-Solutions/3446-find-the-number-of-good-pairs-i/solution.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int numberOfPairs(vector<int>& nums1, vector<int>& nums2, int k) {
+        int ans = 0;
+        for ( auto i : nums1 ) {
+            for ( int j : nums2 ) {
+                if ( i % (j*k) == 0 ) ans++;
+            }
+        }
+        return ans;
+    }
+};
