49af79bbc760d1d99cb9ff3348f3ae0b8c99c7f5
hoangls03 <168925511+hoangls03@users.noreply.github.com>
2024-06-12 01:13:29 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 9.9 MB (36.01%)

---
+++ LeetCode-Solutions/0075-sort-colors/solution.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    void sortColors(vector<int>& nums) {
+        int count0 = 0, count1 = 0;
+        int n = nums.size();
+        for ( int i = 0 ; i < n ; ++i ) {
+            if ( nums[i] == 0 ) count0++;
+            if ( nums[i] == 1 ) count1++;
+        }
+        for ( int i = 0 ; i < n ; ++i ) {
+            if ( count0 > 0 ) {
+                nums[i] = 0;
+                count0--;
+            } else {
+                if ( count1 > 0 ) {
+                    nums[i] = 1;
+                    count1--;
+                } else {
+                    nums[i] = 2;
+                }
+            }
+        }
+    }
+};
