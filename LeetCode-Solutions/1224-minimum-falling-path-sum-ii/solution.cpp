c6698781b2be7c4343b6460784da00d30c8cd311
hoangls03 <168925511+hoangls03@users.noreply.github.com>
2024-07-08 22:33:08 +0000

    [LeetCode Sync] Runtime - 29 ms (81.26%), Memory - 17.1 MB (81.26%)

--- LeetCode-Solutions/1224-minimum-falling-path-sum-ii/solution.cpp
+++ LeetCode-Solutions/1224-minimum-falling-path-sum-ii/solution.cpp
@@ -1,20 +1,33 @@
 class Solution {
 public:
-    int minFallingPathSum(vector<vector<int>>& matrix) {
-        int n = matrix[0].size();
-        vector< vector<int> > dp(n+1,vector<int>(n+1,1e7));
-        int ans = 1e7;
+    int minFallingPathSum(vector<vector<int>>& grid) {
+        int ans = 1e9;
+        int n = grid.size();
+        int dp[n][n];
         for ( int i = 0 ; i < n ; ++i ) {
             for ( int j = 0 ; j < n ; ++j ) {
-                if ( i == 0 ) {
-                    dp[i][j] = matrix[i][j];
+                dp[i][j] = 1e9;
+            }
+        }
+        for ( int i = 0 ; i < n ; ++i ) {
+            int l[n],r[n];
+            if ( i != 0  ) {
+                l[0] = dp[i-1][0];
+                for ( int j = 1 ; j < n ; ++j ) {
+                    l[j] = min(l[j-1],dp[i-1][j]);
                 }
-                else {
-                    for ( int k = 0 ; k < n ; ++k ) {
-                        if ( j != k ) {
-                            dp[i][j] = min(dp[i][j],dp[i-1][k]+ matrix[i][j]);
-                        }
-                    }
+                r[n-1] = dp[i-1][n-1];
+                for ( int j = n - 2 ; j >= 0 ; --j ) {
+                    r[j] = min(r[j+1],dp[i-1][j]);
+                }
+            }
+            for ( int j = 0 ; j < n ; ++j ) {
+                if ( i == 0 ) {
+                    dp[i][j] = grid[i][j];
+                } else {
+                    if ( j == 0 ) dp[i][j] = r[j+1] + grid[i][j];
+                    else if ( j == n - 1 ) dp[i][j] = l[j-1] + grid[i][j];
+                    else dp[i][j] = min(l[j-1],r[j+1]) + grid[i][j];
                 }
                 if ( i == n - 1 ) {
                     ans = min(ans,dp[i][j]);
