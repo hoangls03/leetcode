d68ea6307266a4a90dc8833d78a640ff44c24e69
hoangls03 <168925511+hoangls03@users.noreply.github.com>
2024-07-14 03:32:04 +0000

    [LeetCode Sync] Runtime - 419 ms (47.21%), Memory - 308 MB (6.69%)

---
+++ LeetCode-Solutions/3500-minimum-cost-for-cutting-cake-ii/solution.cpp
@@ -0,0 +1,29 @@
+#define ii pair<long long,long long> 
+#define fi first
+#define se second
+class Solution {
+public:
+    long long minimumCost(int m, int n, vector<int>& horizontalCut, vector<int>& verticalCut) {
+        vector<ii> a;
+        for ( auto i : horizontalCut ) {
+            a.push_back({i,0});
+        }
+        for ( auto i : verticalCut ) {
+            a.push_back({i,1});
+        }
+        sort(a.begin(),a.end());
+        int hor = 1;
+        int ver = 1;
+        long long ans = 0;
+        for ( int i = a.size() - 1 ; i >= 0 ; --i ) {
+            if ( a[i].se == 0 ) {
+                ans += ver*a[i].fi;
+                hor++;
+            } else {
+                ans += hor*a[i].fi;
+                ver++;
+            }
+        }
+        return ans;
+    }
+};
