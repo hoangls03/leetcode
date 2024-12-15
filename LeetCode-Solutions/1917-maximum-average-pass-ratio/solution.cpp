2b24051f5f8dabaecb8d2a4ad74269befe888311
hoangls03 <168925511+hoangls03@users.noreply.github.com>
2024-12-15 03:48:13 +0000

    [LeetCode Sync] Runtime - 395 ms (18.49%), Memory - 109 MB (10.99%)

---
+++ LeetCode-Solutions/1917-maximum-average-pass-ratio/solution.cpp
@@ -0,0 +1,39 @@
+#define ii pair<long long,long long>
+#define fi first
+#define se second
+class Solution {
+public:
+    static bool cmp( ii a, ii b ) {
+        long long u = a.se - a.fi;
+        long long v = a.se * a.se + a.se;
+        long long x = b.se - b.fi;
+        long long y = b.se * b.se + b.se;
+        return ( u*y < x*v );
+    }
+    double maxAverageRatio(vector<vector<int>>& classes, int extraStudents) {
+        priority_queue<ii,vector<ii>,decltype(&cmp)> pq(cmp);
+        for ( auto e : classes ) {
+            pq.push({e[0],e[1]});
+            //cout<<e[0]<<" "<<e[1]<<'\n';
+        }
+        while ( extraStudents ) {
+            auto u = pq.top();
+            pq.pop();
+            u.fi += 1;
+            u.se += 1;
+            pq.push(u);
+            extraStudents--;
+        }
+        double ans = 0;
+        while ( pq.size() ) {
+            auto u = pq.top();
+            double x = u.fi;
+            double y = u.se;
+            ans += x/y;
+            pq.pop();
+        }
+        long long n = classes.size();
+        ans /= n;
+        return ans;
+    }
+};
