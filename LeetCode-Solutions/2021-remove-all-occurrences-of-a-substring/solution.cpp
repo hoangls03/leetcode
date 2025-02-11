46c20c9d21048e3be9c2cbace290d349f7940791
hoangls03 <168925511+hoangls03@users.noreply.github.com>
2025-02-11 00:25:45 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 9.5 MB (66.37%)

---
+++ LeetCode-Solutions/2021-remove-all-occurrences-of-a-substring/solution.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    string removeOccurrences(string s, string part) {
+        while ( s.find(part) != -1 ) {
+            int pos = s.find(part);
+            s.erase(pos,part.size());
+        }
+        return s;
+    }
+};
