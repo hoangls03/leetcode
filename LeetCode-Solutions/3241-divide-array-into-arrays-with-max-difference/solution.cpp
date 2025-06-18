49a3f161752ea04bfd977c68eaf8515c8470b2d3
hoangls03 <168925511+hoangls03@users.noreply.github.com>
2025-06-18 01:57:23 +0000

    [LeetCode Sync] Runtime - 113 ms (14.96%), Memory - 126.3 MB (21.44%)

--- LeetCode-Solutions/3241-divide-array-into-arrays-with-max-difference/solution.cpp
+++ LeetCode-Solutions/3241-divide-array-into-arrays-with-max-difference/solution.cpp
@@ -5,12 +5,8 @@ public:
         sort(nums.begin(),nums.end());
         vector<vector<int>> empty;
         vector<vector<int>> ans(n/3);
-        for ( int i = 0 ; i < n ; ++i ) 
-            cout<<nums[i]<<" ";
-        cout<<'\n';
         for ( int i = 0 ; i < n-2 ; i += 3 ) {
             if ( nums[i+2] - nums[i] > k ) {
-                //cout<<nums[i+2] - nums[i]<<" "<<k<<'\n';
                 return empty;
             }
         }
