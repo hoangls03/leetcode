96960d5212e9f595a53b88f3bff7d0a96fe2a80b
hoangls03 <168925511+hoangls03@users.noreply.github.com>
2025-08-08 20:43:11 +0000

    [LeetCode Sync] Runtime - 140 ms (24.90%), Memory - 8.9 MB (82.16%)

---
+++ LeetCode-Solutions/1617-stone-game-iv/solution.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    bool winnerSquareGame(int n) {
+        vector<bool> dp(n + 1,false);
+        dp[0] = false;
+        for ( int i = 1 ; i <= n ; ++i ) {
+            for ( int j = 1 ; j*j <= i ; ++j ) {
+                dp[i] = dp[i] | (!dp[i - j*j]);
+            }
+        }
+        return dp[n];
+    }
+};
