029489a41f1b45d9843930f752848de8d8e7f72e
hoangls03 <168925511+hoangls03@users.noreply.github.com>
2025-11-14 00:37:35 +0000

    [LeetCode Sync] Runtime - 43 ms (41.39%), Memory - 98.3 MB (5.22%)

---
+++ LeetCode-Solutions/2625-increment-submatrices-by-one/solution.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    vector<vector<int>> rangeAddQueries(int n, vector<vector<int>>& queries) {
+        vector<vector<int>> diff(n + 1,vector<int>(n + 1,0));
+        for ( auto q : queries ) {
+            int row1 = q[0], col1 = q[1];
+            int row2 = q[2], col2 = q[3];
+            diff[row1][col1]++;
+            diff[row2 + 1][col1]--;
+            diff[row1][col2 + 1]--;
+            diff[row2 + 1][col2 + 1]++;
+        }
+        vector mat(n, vector<int>(n));
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                int x1 = (i == 0) ? 0 : mat[i - 1][j];
+                int x2 = (j == 0) ? 0 : mat[i][j - 1];
+                int x3 = (i == 0 || j == 0) ? 0 : mat[i - 1][j - 1];
+                mat[i][j] = diff[i][j] + x1 + x2 - x3;
+            }
+        }
+        return mat;
+    }
+};
