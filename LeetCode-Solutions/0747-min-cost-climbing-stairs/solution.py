69ae34dbde23f79009d38bddb3a1c732a6e52def
hoangls03 <168925511+hoangls03@users.noreply.github.com>
2025-11-29 00:27:55 +0000

    [LeetCode Sync] Runtime - 8 ms (9.98%), Memory - 18 MB (46.99%)

---
+++ LeetCode-Solutions/0747-min-cost-climbing-stairs/solution.py
@@ -0,0 +1,10 @@
+class Solution:
+    def minCostClimbingStairs(self, cost: List[int]) -> int:
+        n = len(cost)
+        dp = [0]*(n + 2)
+        #dp[0] = 0
+        #dp[1] = 0
+        cost.append(0)
+        for i in range(2,n + 2):
+            dp[i] = min(dp[i-1] + cost[i-1],dp[i-2] + cost[i-2])
+        return min(dp[n],dp[n+1])
